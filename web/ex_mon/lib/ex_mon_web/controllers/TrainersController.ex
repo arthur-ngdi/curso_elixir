e9132b5948b1b91759d8b704975ba3d4dc9cf99c
Arthur Ferreira <arthur.ferreira@electroneum.com>
2022-07-29 12:22:10 -0300

    rota e controller de trainer

---
+++ web/ex_mon/lib/ex_mon_web/controllers/TrainersController.ex
@@ -0,0 +1,17 @@
+defmodule ExMonWeb.TrainersController do
+  use ExMonWeb, :controller
+
+  def create(conn, params) do
+    params
+    |> ExMon.create_trainer()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, trainer}, conn) do
+    conn
+    |> put_status(200)
+    |> render("create.json", trainer: trainer)
+  end
+
+
+end
--- web/ex_mon/lib/ex_mon_web/router.ex
+++ web/ex_mon/lib/ex_mon_web/router.ex
@@ -7,6 +7,7 @@ defmodule ExMonWeb.Router do
 
   scope "/api", ExMonWeb do
     pipe_through :api
+    resources "trainers", TrainersController, only: [:create, :show, :delete, :update]
   end
 
   # Enables LiveDashboard only for development
@@ -42,5 +43,6 @@ defmodule ExMonWeb.Router do
     pipe_through :api
 
     get "/", WelcomeController, :index
+    resources "trainers", TrainersController, only: [:create, :show, :delete, :update]
   end
 end
