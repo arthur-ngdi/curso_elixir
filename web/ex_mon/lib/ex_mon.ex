d0f0a8317beda4e62ad5eecd66243493ab0f50cc
Arthur Ferreira <arthur.ferreira@electroneum.com>
2022-12-12 14:24:38 -0300

    excluindo projeto

--- web/ex_mon/lib/ex_mon.ex
+++
@@ -1,5 +0,0 @@
-defmodule ExMon do
-  alias ExMon.Trainer
-
-  defdelegate create_trainer(params), to: Trainer.Create, as: :call
-end
--- web/ex_mon/lib/ex_mon_web/controllers/WelcomeController.ex
+++
@@ -1,9 +0,0 @@
-defmodule ExMonWeb.WelcomeController do
-  use ExMonWeb, :controller
-
-  def index(conn, _params) do
-    text(conn, "Welcome to the ExMon API")
-
-    
-  end
-end
