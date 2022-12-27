187930557439952f8603ddefa353c1bf9ba4cd37
Arthur Ferreira <arthur.ferreira@electroneum.com>
2022-12-27 11:37:35 -0300

    requests criados

--- web/ex_mon/mix.exs
+++ web/ex_mon/mix.exs
@@ -45,7 +45,8 @@ defmodule ExMon.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:argon2_elixir, "~> 2.0"}
+      {:argon2_elixir, "~> 2.0"},
+      {:tesla, "~> 1.4.0"}
     ]
   end
 
